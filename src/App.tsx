2d7234688169fb7715357ca0145c4a2fd8af1f86
Shoaib Sayyed <sayyedshoaib57@gmail.com>
2024-05-05 14:28:47 +0530

    hello world

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+const App: React.FC = () => (
+  <>
+    <div>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </div>
+    <h1>Hello World</h1>
+  </>
+);
+
+export default App;
